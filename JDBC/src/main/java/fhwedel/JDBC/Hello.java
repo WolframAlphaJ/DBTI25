cc1e19110a4978253f809907596da9c2f5c93f27
maxencryptor <147156257+maxencryptor@users.noreply.github.com>
2025-06-04 11:46:39 +0000

    inital

--- JDBC/src/main/java/fhwedel/JDBC/Hello.java
+++ JDBC/src/main/java/fhwedel/JDBC/Hello.java
@@ -1,17 +1,38 @@
 package fhwedel.JDBC;
 
 import java.sql.Connection;
+import java.sql.*;
+
 
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Maxi du kleiner!");
+
+        selectStatement();
+
     }
 
-    public Connection getConnection() {
+    public static Connection getConnection() throws SQLException {
         Connection conn = DriverManager.getConnection(
                 "jdbc:mariadb://localhost:3306/firma",
                 "root", "password");
 
         return conn;
+    }
+
+    public static void selectStatement() {
+        try (Connection conn = getConnection()) {
+            String sql = "SELECT * FROM personal";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println("Personalnummer: " + rs.getInt("pnr") + ", Name: " + rs.getString("name") + ", Vorname: " + rs.getString("vorname") + ", GH_Stufe: " + rs.getString("geh_stufe") +
+                 ", Abteilungsnummer" + rs.getString("abt_nr") + ", Krankenkasse: " + rs.getString("krankenkasse"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
