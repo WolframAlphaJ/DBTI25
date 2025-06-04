aba57453b419ab233642ae1942a81b4f69e542b9
Jannis Schreiner <38886116+WolframAlphaJ@users.noreply.github.com>
2025-06-04 11:33:34 +0000

    kohlmeise

--- JDBC/src/main/java/fhwedel/JDBC/Hello.java
+++ JDBC/src/main/java/fhwedel/JDBC/Hello.java
@@ -1,7 +1,17 @@
 package fhwedel.JDBC;
 
+import java.sql.Connection;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, JDBC world!");
+        System.out.println("Maxi du kleiner!");
+    }
+
+    public Connection getConnection() {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/firma",
+                "root", "password");
+
+        return conn;
     }
 }
